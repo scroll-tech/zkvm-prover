f58b9581598c6d75c339fbd4f8067d0eb537516c
Rohit Narurkar <rohit.narurkar@proton.me>
2025-01-21 23:38:18 +0000

    note busy-time from span:close

--- crates/integration/src/lib.rs
+++ crates/integration/src/lib.rs
@@ -132,7 +132,7 @@ impl<T: Clone, P: Clone> ProveVerifyOutcome<T, P> {
 pub fn setup() -> eyre::Result<()> {
     tracing_subscriber::fmt()
         .with_env_filter(EnvFilter::from_default_env())
-        .with_span_events(FmtSpan::ACTIVE)
+        .with_span_events(FmtSpan::CLOSE)
         .pretty()
         .init();
 
