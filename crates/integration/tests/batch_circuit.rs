bab8820af7b8b1558bae07e958543d9d0db2e76b
noelwei <fan@scroll.io>
2025-01-22 21:48:52 +0900

    add simple execution test for batch

--- crates/integration/tests/batch_circuit.rs
+++ crates/integration/tests/batch_circuit.rs
@@ -2,6 +2,62 @@ use scroll_zkvm_integration::{
     prove_verify_multi, prove_verify_single, setup,
     testers::{batch::BatchProverTester, chunk::MultiChunkProverTester},
 };
+use scroll_zkvm_prover::{utils::read_json, setup::read_app_config, task::batch::BatchProvingTask};
+use openvm_sdk::{
+    config::{AggConfig, AppConfig, SdkVmConfig},
+    prover::AppProver,
+    Sdk, StdIn,
+};
+use openvm_transpiler::{openvm_platform::memory::MEM_SIZE, elf::Elf};
+use tracing::info;
+
+#[test]
+fn batch_simple_execution() -> eyre::Result<()> {
+    setup()?;
+
+    // read task
+    let task : BatchProvingTask = read_json("testdata/batch-task-with-blob.json")?;
+    info!("benching task for batch {}", task.batch_header.batch_index);
+
+    // suppose we are under `integration` path
+    let app_config = read_app_config("../circuits/batch-circuit/openvm.toml")?;
+    let vm_config = app_config.app_vm_config;
+
+    // ANCHOR_END: vm_config
+
+    // to import example guest code in crate replace `target_path` for:
+    // ```
+    // use std::path::PathBuf;
+    //
+    // let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).to_path_buf();
+    // path.push("guest");
+    // let target_path = path.to_str().unwrap();
+    // ```
+    // ANCHOR: build
+    // 1. Build the VmConfig with the extensions needed.
+    let sdk = Sdk;
+    // 2a. Build the ELF with guest options and a target filter (skipped, simply read elf).
+
+
+    let elf_bytes = std::fs::read("../../target/riscv32im-risc0-zkvm-elf/release/batch-circuit")?;
+    let elf = Elf::decode(&elf_bytes, MEM_SIZE as u32)?;    
+    // ANCHOR_END: build
+
+    // ANCHOR: transpilation
+    // 3. Transpile the ELF into a VmExe
+    let exe = sdk.transpile(elf, vm_config.transpiler())?;
+
+    // ANCHOR: execution
+    // 4. Format your input into StdIn
+    let mut stdin = StdIn::default();
+    stdin.write_bytes(&task.serialized_into());
+
+    let start_t = std::time::Instant::now();
+    let output = sdk.execute(exe.clone(), vm_config.clone(), stdin.clone())?;
+    info!("complete in {:?}, public values output: {:?}", start_t.elapsed(), output);
+
+    Ok(())
+}
 
 #[test]
 fn setup_prove_verify() -> eyre::Result<()> {
---
+++ crates/prover/src/task/batch.rs
@@ -0,0 +1,87 @@
+use circuit_input_types::batch::{ChunkInfo, BatchHeaderV3, BatchTask};
+use crate::utils::base64;
+use serde::{Deserialize, Serialize};
+
+pub const MAX_AGG_CHUNKS: usize = 45;
+
+// we grap all definations from zkevm-circuit to parse the json of batch task
+
+/// The innermost SNARK belongs to the following variants.
+#[derive(Copy, Clone, Debug, Deserialize, Serialize, PartialEq)]
+pub enum ChunkKind {
+    /// halo2-based SuperCircuit.
+    Halo2,
+    /// sp1-based STARK with a halo2-backend.
+    Sp1,
+}
+
+impl Default for ChunkKind {
+    fn default() -> Self {
+        Self::Halo2
+    }
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct ChunkProofV2Metadata {
+    /// The [`Protocol`][snark_verifier::Protocol] for the SNARK construction for the chunk proof.
+    #[serde(with = "base64")]
+    pub protocol: Vec<u8>,
+    /// The chunk proof can be for either the halo2 or sp1 routes.
+    #[serde(default)]
+    pub chunk_kind: ChunkKind,
+    /// The EVM execution traces as a result of executing all txs in the chunk.
+    pub chunk_info: ChunkInfo,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct ChunkProofV2 {
+    /// The inner data that differs between chunk proofs, batch proofs and bundle proofs.
+    /// TODO: do we still need metadata? or simply the chunk info?
+    #[serde(flatten)]
+    pub inner: ChunkProofV2Metadata,
+    /// The raw bytes of the proof in the [`Snark`].
+    ///
+    /// Serialized using base64 format in order to not bloat the JSON-encoded proof dump.
+    #[serde(with = "base64")]
+    pub proof: Vec<u8>,
+    /// The public values, aka instances of this [`Snark`].
+    #[serde(with = "base64")]
+    pub instances: Vec<u8>,
+    /// The raw bytes of the [`VerifyingKey`] of the [`Circuit`] used to generate the [`Snark`].
+    #[serde(with = "base64")]
+    pub vk: Vec<u8>,
+    /// The git ref of the codebase.
+    ///
+    /// Generally useful for debug reasons to know the exact commit using which this proof was
+    /// generated.
+    pub git_version: String,
+}
+
+
+/// Defines a proving task for batch proof generation.
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct BatchProvingTask {
+    /// Chunk proofs for the contiguous list of chunks within the batch.
+    pub chunk_proofs: Vec<ChunkProofV2>,
+    /// The [`BatchHeader`], as computed on-chain for this batch.
+    ///
+    /// Ref: https://github.com/scroll-tech/scroll-contracts/blob/2ac4f3f7e090d7127db4b13b3627cb3ce2d762bc/src/libraries/codec/BatchHeaderV3Codec.sol
+    pub batch_header: BatchHeaderV3,
+    /// The bytes encoding the batch data that will finally be published on-chain in the form of an
+    /// EIP-4844 blob.
+    #[serde(with = "base64")]
+    pub blob_bytes: Vec<u8>,
+}
+
+impl BatchProvingTask {
+    pub fn serialized_into(self) -> rkyv::util::AlignedVec {
+        let input_task = BatchTask {
+            chunks_info: self.chunk_proofs.
+                iter().map(|chunk_proofs|chunk_proofs.inner.chunk_info.clone())
+                .collect(),
+            blob_bytes: self.blob_bytes,
+            header_v3: Some(self.batch_header),
+        };
+        rkyv::to_bytes::<rkyv::rancor::Error>(&input_task).unwrap()
+    }   
+}
