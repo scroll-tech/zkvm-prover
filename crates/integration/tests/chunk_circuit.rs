8807ed4d8c43c91389f46f2f237ab20030821b33
Rohit Narurkar <rohit.narurkar@proton.me>
2025-01-22 01:53:22 +0000

    multi-chunk test

--- crates/integration/tests/chunk_circuit.rs
+++ crates/integration/tests/chunk_circuit.rs
@@ -1,10 +1,22 @@
-use scroll_zkvm_integration::{prove_verify_single, setup, testers::chunk::ChunkProverTester};
+use scroll_zkvm_integration::{
+    prove_verify_multi, prove_verify_single, setup,
+    testers::chunk::{ChunkProverTester, MultiChunkProverTester},
+};
 
 #[test]
 fn setup_prove_verify() -> eyre::Result<()> {
     setup()?;
 
     let _outcome = prove_verify_single::<ChunkProverTester>(None)?;
+
+    Ok(())
+}
+
+#[test]
+fn multi_chunk() -> eyre::Result<()> {
+    setup()?;
+
+    let _outcome = prove_verify_multi::<MultiChunkProverTester>(None)?;
 
     Ok(())
 }
