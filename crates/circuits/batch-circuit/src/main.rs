607bdadc923236c6ef6110a149d0f8332e80107a
Zhuo Zhang <mycinbrin@gmail.com>
2025-01-22 09:29:39 +0000

    add init aggregation code, fail due to version mismatch?

--- crates/circuits/batch-circuit/src/main.rs
+++ crates/circuits/batch-circuit/src/main.rs
@@ -22,10 +22,71 @@ openvm_algebra_guest::moduli_setup::moduli_init! {
 } 
 
 openvm::entry!(main);
+
+const EXE_COMMIT : [u32; 8] =[396649651, 1175086036, 1682626845, 471855974, 1659938811, 1981570609, 805067545, 1640289616];
+const LEAF_COMMIT: [u32; 8] = [505034789, 682334490, 407062982, 1227826652, 298205975, 1959777750, 1633765816, 97452666];
+
+
+// will terminate inside if fail
+fn exec_kernel(input: &[u32], expect_output: &[u32]) {
+    let mut input_ptr: *const u32 = input.as_ptr();
+    let mut output_ptr: *const u32 = expect_output.as_ptr();
+    let mut buf1: u32 = 0;
+    let mut buf2: u32 = 0;
+    #[cfg(all(target_os = "zkvm", target_arch = "riscv32"))]
+    unsafe {
+        std::arch::asm!(
+            include_str!("../../../tools/generate-verifier-asm/root_verifier.asm"),
+            inout("x28") input_ptr,
+            inout("x29") output_ptr,
+            inout("x30") buf1,
+            inout("x31") buf2,
+        )
+    }
+}
+
+fn verify_chunk_proof(flatten_proof: &[u32], public_inputs: &[u32]) {
+    let mut full_pi = vec![];
+    let default_pi_len = 32;
+    assert_eq!(default_pi_len, public_inputs.len());
+    full_pi.extend(EXE_COMMIT);
+    full_pi.extend(LEAF_COMMIT);
+    full_pi.extend_from_slice(public_inputs);
+    exec_kernel(flatten_proof, &full_pi);
+    println!("verified chunk proof successfully");
+}
+
+#[derive(serde::Deserialize)]
+struct FlattenRootProof {
+    flatten_proof: Vec<u32>,
+    public_values: Vec<u32>,
+}
+
+fn main() {
+    let raw_input: Vec<u8> = openvm::io::read_vec();
+    let input: FlattenRootProof = bitcode::deserialize(&raw_input).expect("decode");
+
+    println!(
+        "input.flatten_proof[..30]: {:?}",
+        &input.flatten_proof[..30]
+    );
+    println!(
+        "input.public_values[..30]: {:?}",
+        &input.public_values[..30]
+    );
+    verify_chunk_proof(&input.flatten_proof, &input.public_values);
+
+}
+
+/* 
 fn main() {
 
+
     let input_data = io::read_vec();
+
+
     
+
     let task = access::<ArchivedBatchTask, BoxedError>(&input_data).unwrap();
 
     let header_v3 = task.header_v3.as_ref().unwrap();
@@ -43,4 +104,6 @@ fn main() {
     );
 //    println!("data len {}", data.len());
 //    process(&data).unwrap();
+
 }
+*/
