52075021fffff975ddeaf66fbb70c52314af7da1
noelwei <fan@scroll.io>
2025-01-28 20:48:11 +0900

    purge simple execution

--- crates/integration/tests/batch_circuit.rs
+++ crates/integration/tests/batch_circuit.rs
@@ -1,94 +1,24 @@
 use scroll_zkvm_integration::{
-    prove_verify_multi, prove_verify_single, setup_logger,
+    ProverTester, prove_verify_multi, prove_verify_single, setup_logger,
     testers::{
         batch::{BatchProverTester, MultiBatchProverTester},
         chunk::MultiChunkProverTester,
     },
-    utils::build_batch_task,
-};
-use scroll_zkvm_prover::{
-    ChunkProof,
-    task::{batch::BatchProvingTask, chunk::ChunkProvingTask},
-    utils::read_json,
 };
 
 #[test]
-fn batch_simple_execution() -> eyre::Result<()> {
-    use BatchProverTester as T;
-    use scroll_zkvm_integration::ProverTester;
-    use std::str::FromStr;
-
+fn test_execute() -> eyre::Result<()> {
     setup_logger()?;
 
-    // Setup test-run directories.
-    T::setup()?;
+    MultiBatchProverTester::setup()?;
 
-    // Build the ELF binary from the circuit program.
-    let elf = T::build()?;
+    let elf = MultiBatchProverTester::build()?;
 
-    // Transpile the ELF into a VmExe.
-    let (app_config, exe_path) = T::transpile(elf)?;
-    // read task
-    let task: BatchProvingTask = {
-        let block_dir = "testdata/";
+    let (app_config, exe_path) = MultiBatchProverTester::transpile(elf)?;
 
-        let blk_names = [
-            "12508460.json",
-            "12508461.json",
-            "12508462.json",
-            "12508463.json",
-        ];
+    let task = MultiBatchProverTester::gen_proving_task()?;
 
-        let blk_witness =
-            |n| read_json::<_, sbv::primitives::types::BlockWitness>(format!("{block_dir}/{}", n));
-
-        // manual match to chunk tasks
-        let chk_task = [
-            ChunkProvingTask {
-                block_witnesses: vec![blk_witness(blk_names[0])?],
-            },
-            ChunkProvingTask {
-                block_witnesses: vec![blk_witness(blk_names[1])?],
-            },
-            ChunkProvingTask {
-                block_witnesses: vec![blk_witness(blk_names[2])?, blk_witness(blk_names[3])?],
-            },
-        ];
-
-        // let chk_task = [ChunkProvingTask {
-        //     block_witnesses: Vec::from(blk_names.map(blk_witness).map(Result::unwrap)),
-        // }];
-
-        let proof_dir = "testdata/chunk";
-        let pathes = [
-            "chunk-12508460-12508460.json",
-            "chunk-12508461-12508461.json",
-            "chunk-12508462-12508463.json",
-        ];
-        // let pathes = [
-        //     "chunk-12508460-12508463.json",
-        // ];
-        let chunk_proofs = pathes.map(|p| {
-            let p = format!("{proof_dir}/{p}");
-            let mut proof = ChunkProof::from_json(p).unwrap();
-            proof.metadata.chunk_info.withdraw_root = Some(
-                sbv::primitives::B256::from_str(
-                    "0x7ed4c7d56e2ed40f65d25eecbb0110f3b3f4db68e87700287c7e0cedcb68272c",
-                )
-                .unwrap(),
-            );
-            proof
-        });
-        build_batch_task(
-            &chk_task,
-            &chunk_proofs,
-            scroll_zkvm_circuit_input_types::batch::MAX_AGG_CHUNKS,
-            Default::default(),
-        )
-        // read_json("testdata/batch-task-with-blob.json")?;
-    };
-
-    T::execute(app_config, &task, exe_path)?;
+    MultiBatchProverTester::execute(app_config.clone(), &task, exe_path.clone())?;
 
     Ok(())
 }
