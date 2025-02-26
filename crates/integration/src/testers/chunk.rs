7ee36092e572cceb16c3817f8c035f6d5bdb4f47
Rohit Narurkar <rohit.narurkar@proton.me>
2025-02-26 17:21:42 +0000

    load from witnesses

--- crates/integration/src/testers/chunk.rs
+++ crates/integration/src/testers/chunk.rs
@@ -14,6 +14,12 @@ fn read_block_witness(block_n: usize) -> eyre::Result<BlockWitness> {
     Ok(serde_json::from_reader::<_, BlockWitness>(witness)?)
 }
 
+fn read_block_witness_str(block_n: &str) -> eyre::Result<BlockWitness> {
+    let path_witness = Path::new(PATH_TESTDATA).join("witnesses").join(block_n);
+    let witness = File::open(&path_witness)?;
+    Ok(serde_json::from_reader::<_, BlockWitness>(witness)?)
+}
+
 pub struct ChunkProverTester;
 
 impl ProverTester for ChunkProverTester {
@@ -52,19 +58,70 @@ impl ProverTester for MultiChunkProverTester {
     fn gen_multi_proving_tasks() -> eyre::Result<Vec<<Self::Prover as ProverType>::ProvingTask>> {
         Ok(vec![
             ChunkProvingTask {
-                block_witnesses: (12508460usize..=12508460)
-                    .map(read_block_witness)
-                    .collect::<eyre::Result<Vec<BlockWitness>>>()?,
+                block_witnesses: vec![read_block_witness_str("0x2.json")?],
             },
             ChunkProvingTask {
-                block_witnesses: (12508461usize..=12508461)
-                    .map(read_block_witness)
-                    .collect::<eyre::Result<Vec<BlockWitness>>>()?,
+                block_witnesses: vec![read_block_witness_str("0x3.json")?],
             },
             ChunkProvingTask {
-                block_witnesses: (12508462usize..=12508463)
-                    .map(read_block_witness)
-                    .collect::<eyre::Result<Vec<BlockWitness>>>()?,
+                block_witnesses: vec![read_block_witness_str("0x4.json")?],
+            },
+            ChunkProvingTask {
+                block_witnesses: vec![read_block_witness_str("0x5.json")?],
+            },
+            ChunkProvingTask {
+                block_witnesses: vec![read_block_witness_str("0x6.json")?],
+            },
+            ChunkProvingTask {
+                block_witnesses: vec![
+                    read_block_witness_str("0x7.json")?,
+                    read_block_witness_str("0x8.json")?,
+                ],
+            },
+            ChunkProvingTask {
+                block_witnesses: vec![read_block_witness_str("0x9.json")?],
+            },
+            ChunkProvingTask {
+                block_witnesses: vec![read_block_witness_str("0xA.json")?],
+            },
+            ChunkProvingTask {
+                block_witnesses: vec![read_block_witness_str("0xB.json")?],
+            },
+            ChunkProvingTask {
+                block_witnesses: vec![read_block_witness_str("0xC.json")?],
+            },
+            ChunkProvingTask {
+                block_witnesses: vec![
+                    read_block_witness_str("0xD.json")?,
+                    read_block_witness_str("0xE.json")?,
+                ],
+            },
+            ChunkProvingTask {
+                block_witnesses: vec![
+                    read_block_witness_str("0xF.json")?,
+                    read_block_witness_str("0x10.json")?,
+                    read_block_witness_str("0x11.json")?,
+                    read_block_witness_str("0x12.json")?,
+                    read_block_witness_str("0x13.json")?,
+                ],
+            },
+            ChunkProvingTask {
+                block_witnesses: vec![
+                    read_block_witness_str("0x14.json")?,
+                    read_block_witness_str("0x15.json")?,
+                    read_block_witness_str("0x16.json")?,
+                    read_block_witness_str("0x17.json")?,
+                    read_block_witness_str("0x18.json")?,
+                ],
+            },
+            ChunkProvingTask {
+                block_witnesses: vec![
+                    read_block_witness_str("0x19.json")?,
+                    read_block_witness_str("0x1A.json")?,
+                    read_block_witness_str("0x1B.json")?,
+                    read_block_witness_str("0x1C.json")?,
+                    read_block_witness_str("0x1D.json")?,
+                ],
             },
         ])
     }
