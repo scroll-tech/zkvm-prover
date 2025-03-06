96bcfbbffd15a9c3c1bcd348aa8e48621e5f6cd4
Rohit Narurkar <rohit.narurkar@proton.me>
2025-03-06 09:08:14 +0000

    copy release assets, prepare for integration tests

--- download-release.sh
+++ download-release.sh
@@ -9,3 +9,8 @@ fi
 mkdir -p releases
 
 aws --profile default s3 cp s3://circuit-release/scroll-zkvm/releases/$SCROLL_ZKVM_VERSION releases/$SCROLL_ZKVM_VERSION --recursive
+
+# copy release assets to correct path in workdir, in preparation for integration tests
+cp releases/$SCROLL_ZKVM_VERSION/chunk/app.vmexe crates/circuits/chunk-circuit/openvm/app.vmexe
+cp releases/$SCROLL_ZKVM_VERSION/batch/app.vmexe crates/circuits/batch-circuit/openvm/app.vmexe
+cp releases/$SCROLL_ZKVM_VERSION/bundle/app.vmexe crates/circuits/bundle-circuit/openvm/app.vmexe
