de51a789ae01bc02e19e2e26a751b9463200254f
Rohit Narurkar <rohit.narurkar@proton.me>
2025-03-06 14:08:02 +0000

    chore: add script to fetch artifacts for current HEAD

---
+++ download-artifacts.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# release version
+if [ -z "${COMMIT_REF}" ]; then
+  echo "COMMIT_REF not set => get HEAD"
+  COMMIT_REF=$(git rev-parse HEAD)
+fi
+
+# chunk-circuit exe
+wget https://circuit-release.s3.us-west-2.amazonaws.com/scroll-zkvm/$COMMIT_REF/chunk/app.vmexe -O crates/circuits/chunk-circuit/openvm/app.vmexe
+
+# batch-circuit exe
+wget https://circuit-release.s3.us-west-2.amazonaws.com/scroll-zkvm/$COMMIT_REF/batch/app.vmexe -O crates/circuits/batch-circuit/openvm/app.vmexe
+
+# bundle-circuit exe
+wget https://circuit-release.s3.us-west-2.amazonaws.com/scroll-zkvm/$COMMIT_REF/bundle/app.vmexe -O crates/circuits/bundle-circuit/openvm/app.vmexe
