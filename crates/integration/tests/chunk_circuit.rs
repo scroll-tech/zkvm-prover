1d1385315a9bf88e71d532ec8113b40095df88e4
noelwei <fan@scroll.io>
2025-03-06 16:17:22 +0900

    revert profiling refactors

--- crates/integration/tests/chunk_circuit.rs
+++ crates/integration/tests/chunk_circuit.rs
@@ -1,66 +1,19 @@
-use std::path::Path;
-
 use scroll_zkvm_integration::{
     ProverTester, prove_verify_multi, prove_verify_single,
-    testers::{
-        PATH_TESTDATA,
-        chunk::{ChunkProverTester, MultiChunkProverTester, read_block_witness},
-    },
+    testers::chunk::{ChunkProverTester, MultiChunkProverTester},
 };
-use scroll_zkvm_prover::{
-    ChunkProver, ChunkProverType, ProverType,
-    task::{ProvingTask, chunk::ChunkProvingTask},
-};
-
-#[test]
-fn test_cycle() -> eyre::Result<()> {
-    ChunkProverTester::setup()?;
-
-    let (path_app_config, _app_config, path_exe) = ChunkProverTester::load()?;
-    // use rayon::prelude::*;
-
-    let blocks = 1..=8;
-    blocks.into_iter().try_for_each(|blk| -> eyre::Result<()> {
-        let task = ChunkProvingTask {
-            block_witnesses: vec![read_block_witness(blk, Path::new(PATH_TESTDATA))?],
-            prev_msg_queue_hash: Default::default(),
-        };
-
-        let stats = task.stats();
-        ChunkProverType::metadata_with_prechecks(&task)?;
-        let prover = ChunkProver::setup(&path_exe, &path_app_config, None, Default::default())?;
-        let profile = false;
-        let (cycle_count, segments) = prover.execute_guest(&task.build_guest_input()?, profile)?;
-        if profile {
-            prover.build_executor_results(segments, profile);
-        }
-        let cycle_per_gas = cycle_count / stats.total_gas_used;
-        println!("chunk stats {:#?}", stats);
-        println!(
-            "blk {}: total cycle {}, gas {}, cycle per gas {}",
-            blk, cycle_count, stats.total_gas_used, cycle_per_gas
-        );
-        assert!(cycle_per_gas < 30);
-        Ok(())
-    })?;
-    Ok(())
-}
+use scroll_zkvm_prover::task::ProvingTask;
 
 #[test]
 fn test_execute() -> eyre::Result<()> {
-    ChunkProverTester::setup()?;
+    MultiChunkProverTester::setup()?;
 
-    let (path_app_config, _app_config, path_exe) = ChunkProverTester::load()?;
-    let task = ChunkProverTester::gen_proving_task()?;
-    let stats = task.stats();
-    println!("chunk stats {:#?}", stats);
-    ChunkProverType::metadata_with_prechecks(&task)?;
-    let prover = ChunkProver::setup(&path_exe, &path_app_config, None, Default::default())?;
-    let profile = false;
-    let (cycle_count, _) = prover.execute_guest(&task.build_guest_input()?, profile)?;
-    let cycle_per_gas = cycle_count / stats.total_gas_used;
-    println!("total cycle count {}", cycle_count);
-    println!("cycle count per gas {}", cycle_per_gas);
+    let (_, app_config, exe_path) = MultiChunkProverTester::load()?;
+
+    for task in MultiChunkProverTester::gen_multi_proving_tasks()? {
+        MultiChunkProverTester::execute(app_config.clone(), &task, exe_path.clone())?;
+    }
+
     Ok(())
 }
 
@@ -92,7 +45,9 @@ fn guest_profiling() -> eyre::Result<()> {
 
     let task = ChunkProverTester::gen_proving_task()?;
     let stdin = task.build_guest_input()?;
-    let (total_cycles, _) = chunk_prover.execute_guest(&stdin, true)?;
+    let (total_cycles, _) = chunk_prover
+        .execute_guest(&stdin)?
+        .ok_or(eyre::eyre!("execute_guest returned None"))?;
 
     println!(
         "scroll-zkvm-integration(chunk-circuit): total cycles = {:?}",
--- crates/prover/src/prover/mod.rs
+++ crates/prover/src/prover/mod.rs
@@ -1,5 +1,4 @@
 use std::{
-    collections::BTreeMap,
     marker::PhantomData,
     path::{Path, PathBuf},
     sync::Arc,
@@ -8,7 +7,7 @@ use std::{
 use metrics_util::{MetricKind, debugging::DebugValue};
 use once_cell::sync::Lazy;
 use openvm_circuit::{
-    arch::{ExecutionSegment, SingleSegmentVmExecutor, VmExecutor, VmExecutorResult},
+    arch::{SingleSegmentVmExecutor, VmExecutor, VmExecutorResult},
     system::{memory::tree::public_values::extract_public_values, program::trace::VmCommittedExe},
 };
 use openvm_native_recursion::{
@@ -403,30 +402,35 @@ impl<Type: ProverType> Prover<Type> {
         Ok(())
     }
 
-    /// Build the executor results.
-    /// Yes it is a bit wired that it has the "profile" parameter.
-    /// But now `segment.finalize_metric` is not public, so we can only collect
-    /// metrics during `segment.generate_proof_input`.
-    /// TODO: when `segment.finalize_metric` becomes public, refactor here.
-    pub fn build_executor_results(
+    /// Execute the guest program to get the cycle count.
+    ///
+    /// Runs only if the GUEST_PROFILING environment variable has been set to "true".
+    pub fn execute_guest(
         &self,
-        mut segments: Vec<ExecutionSegment<F, SdkVmConfig>>,
-        profile: bool,
-    ) -> VmExecutorResult<SC> {
+        stdin: &StdIn,
+    ) -> Result<Option<(u64, VmExecutorResult<SC>)>, Error> {
+        use openvm_circuit::arch::VmConfig;
+        use openvm_stark_sdk::openvm_stark_backend::p3_field::Field;
+
+        if std::env::var("GUEST_PROFILING").as_deref() != Ok("true") {
+            return Ok(None);
+        }
+
+        let config = self.app_pk.app_vm_pk.vm_config.clone();
+        let system_config = <SdkVmConfig as VmConfig<F>>::system(&config);
+        let vm = VmExecutor::new(config.clone());
+
+        let mut segments = vm
+            .execute_segments(self.app_committed_exe.exe.clone(), stdin.clone())
+            .map_err(|e| Error::GenProof(e.to_string()))?;
         let final_memory = std::mem::take(&mut segments.last_mut().unwrap().final_memory);
         let mut metric_snapshots = vec![];
-
         let executor_result = VmExecutorResult {
             per_segment: segments
                 .into_iter()
                 .map(|seg| {
-                    use metrics_tracing_context::TracingContextLayer;
-                    use metrics_util::layers::Layer;
                     let recorder = metrics_util::debugging::DebuggingRecorder::new();
                     let snapshotter = recorder.snapshotter();
-                    // zzhang: i don't know why this TracingContextLayer is needed, but i copied it from
-                    // "stark-backend" repo.
-                    let recorder = TracingContextLayer::all().layer(recorder);
                     let seg_proof_input = metrics::with_local_recorder(&recorder, || {
                         seg.generate_proof_input(Some(
                             self.app_committed_exe.committed_program.clone(),
@@ -439,93 +443,10 @@ impl<Type: ProverType> Prover<Type> {
             final_memory,
         };
 
-        let mut counter_sum = BTreeMap::<String, BTreeMap<String, u64>>::new();
-        for (idx, metric_snapshot) in metric_snapshots.into_iter().enumerate() {
-            let metrics = metric_snapshot.into_vec();
-            for (ckey, _, _, value) in metrics {
-                match ckey.kind() {
-                    MetricKind::Counter => {
-                        let value = match value {
-                            DebugValue::Counter(v) => v,
-                            _ => panic!("unexpected value type"),
-                        };
-                        tracing::debug!(
-                            "metric of segment {}: {}=>{}, {ckey:?}",
-                            idx,
-                            ckey.key().name(),
-                            value
-                        );
-                        let label = ckey
-                            .key()
-                            .labels()
-                            .map(|l| l.value())
-                            .filter(|l| !l.is_empty())
-                            .collect::<Vec<_>>()
-                            .join("|");
-                        let counter_name = ckey.key().name().to_string();
-                        let counter_map = counter_sum.entry(counter_name).or_default();
-                        let counter_value = counter_map.entry(label).or_insert(0);
-                        *counter_value += value;
-                    }
-                    MetricKind::Gauge => {}
-                    MetricKind::Histogram => {}
-                }
-            }
-        }
-
-        if profile {
-            let mut bench_report = String::new();
-            use std::fmt::Write;
-            writeln!(&mut bench_report, "guest profiling:").unwrap();
-            for (counter_name, values) in counter_sum.iter() {
-                writeln!(&mut bench_report, "{counter_name}:").unwrap();
-                let mut sorted_values: Vec<_> = values.iter().collect();
-                sorted_values.sort_by(|a, b| b.1.cmp(a.1));
-                for (label, value) in sorted_values {
-                    writeln!(&mut bench_report, "  {label}\t{value}").unwrap();
-                }
-                writeln!(&mut bench_report).unwrap();
-            }
-            println!("{}", bench_report);
-        }
-
-        executor_result
-    }
-
-    /// Execute the guest program to get the cycle count.
-    ///
-    /// If the GUEST_PROFILING environment variable has been set to "true",
-    /// row_usage/cell_usage/counter_per_op metrics are also collected.
-    pub fn execute_guest(
-        &self,
-        stdin: &StdIn,
-        profile: bool,
-    ) -> Result<(u64, Vec<ExecutionSegment<F, SdkVmConfig>>), Error> {
-        use openvm_circuit::arch::VmConfig;
-        use openvm_stark_sdk::openvm_stark_backend::p3_field::Field;
-
-        let mut config = self.app_pk.app_vm_pk.vm_config.clone();
-        if profile {
-            config.system.config = config.system.config.with_profiling();
-        }
-        let vm = VmExecutor::new(config.clone());
-
-        let segments = vm
-            .execute_segments(self.app_committed_exe.exe.clone(), stdin.clone())
-            .map_err(|e| Error::GenProof(e.to_string()))?;
-        let total_cycle = segments
-            .iter()
-            .map(|seg| seg.metrics.cycle_count)
-            .sum::<usize>() as u64;
-        tracing::info!(name: "segment length", segment_len = segments.len());
-        tracing::info!(name: "total cycle", ?total_cycle);
+        tracing::debug!(name: "segment length", segment_len = executor_result.per_segment.len());
 
         // extract and check public values
-        let final_memory = segments
-            .last()
-            .and_then(|x| x.final_memory.as_ref())
-            .unwrap();
-        let system_config = <SdkVmConfig as VmConfig<F>>::system(&config);
+        let final_memory = executor_result.final_memory.as_ref().unwrap();
         let public_values: Vec<F> = extract_public_values(
             &system_config.memory_config.memory_dimensions(),
             system_config.num_public_values,
@@ -536,7 +457,38 @@ impl<Type: ProverType> Prover<Type> {
             return Err(Error::GenProof("public_values are all 0s".to_string()));
         }
 
-        Ok((total_cycle, segments))
+        let mut counter_sum = std::collections::HashMap::<String, u64>::new();
+        for (idx, metric_snapshot) in metric_snapshots.into_iter().enumerate() {
+            let metrics = metric_snapshot.into_vec();
+            for (ckey, _, _, value) in metrics {
+                match ckey.kind() {
+                    MetricKind::Gauge => {}
+                    MetricKind::Counter => {
+                        let value = match value {
+                            DebugValue::Counter(v) => v,
+                            _ => panic!("unexpected value type"),
+                        };
+                        tracing::debug!(
+                            "metric of segment {}: {}=>{}",
+                            idx,
+                            ckey.key().name(),
+                            value
+                        );
+                        // add to `counter_sum`
+                        let counter_name = ckey.key().name().to_string();
+                        let counter_value = counter_sum.entry(counter_name).or_insert(0);
+                        *counter_value += value;
+                    }
+                    MetricKind::Histogram => {}
+                }
+            }
+        }
+        for (name, value) in counter_sum.iter() {
+            tracing::debug!("metric of all segments: {}=>{}", name, value);
+        }
+
+        let total_cycle = counter_sum.get("total_cycles").cloned().unwrap_or(0);
+        Ok(Some((total_cycle, executor_result)))
     }
 
     /// File descriptor for the proof saved to disc.
@@ -554,15 +506,8 @@ impl<Type: ProverType> Prover<Type> {
             .build_guest_input()
             .map_err(|e| Error::GenProof(e.to_string()))?;
 
-        let mock_prove = std::env::var("MOCK_PROVE").as_deref() == Ok("true");
-        let guest_profiling = std::env::var("GUEST_PROFILING").as_deref() == Ok("true");
-        // Here we always do an execution of the guest program to get the cycle count and do precheck before proving.
-        let (_cycle_count, segments) = self.execute_guest(&stdin, guest_profiling)?;
-        if mock_prove || guest_profiling {
-            let executor_result = self.build_executor_results(segments, guest_profiling);
-            if mock_prove {
-                self.mock_prove(executor_result)?;
-            }
+        if let Some((_cycle_count, executor_result)) = self.execute_guest(&stdin)? {
+            self.mock_prove_if_needed(executor_result)?;
         }
 
         let task_id = task.identifier();
@@ -578,7 +523,7 @@ impl<Type: ProverType> Prover<Type> {
     }
 
     /// Runs only if the MOCK_PROVE environment variable has been set to "true".
-    fn mock_prove(&self, result: VmExecutorResult<SC>) -> Result<(), Error> {
+    fn mock_prove_if_needed(&self, result: VmExecutorResult<SC>) -> Result<(), Error> {
         use openvm_circuit::arch::VmConfig;
         use openvm_stark_sdk::{
             config::baby_bear_poseidon2::BabyBearPoseidon2Engine, engine::StarkFriEngine,
