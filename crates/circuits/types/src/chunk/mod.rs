de4858a34db0e8720d3f5f3565281ee4c27eec1e
noelwei <fan@scroll.io>
2025-03-10 13:36:31 +0900

    trivial fixes according to review

--- crates/circuits/types/src/chunk/mod.rs
+++ crates/circuits/types/src/chunk/mod.rs
@@ -1,8 +1,6 @@
-//#[cfg(not(feature = "euclidv2"))]
 pub mod public_inputs_legacy;
 #[cfg(not(feature = "euclidv2"))]
 pub use public_inputs_legacy::{ArchivedChunkInfo, ChunkInfo};
-//#[cfg(feature = "euclidv2")]
 pub mod public_inputs_euclidv2;
 #[cfg(feature = "euclidv2")]
 pub use public_inputs_euclidv2::{ArchivedChunkInfo, BlockContextV2, ChunkInfo, SIZE_BLOCK_CTX};
