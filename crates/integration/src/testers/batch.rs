a7e90bf30a931561eb431113311b3d24337b6137
noelwei <fan@scroll.io>
2025-03-14 12:31:33 +0900

    better structure for testdata (WIP)

--- crates/integration/src/testers/batch.rs
+++ crates/integration/src/testers/batch.rs
@@ -2,7 +2,7 @@ use std::path::Path;
 
 use scroll_zkvm_prover::{BatchProverType, ProverType, utils::read_json_deep};
 
-use crate::{ProverTester, testers::PATH_TESTDATA};
+use crate::{ProverTester, testers::PATH_TESTDATA, utils::phase_base_directory};
 
 pub struct BatchProverTester;
 
@@ -14,12 +14,12 @@ impl ProverTester for BatchProverTester {
     const DIR_ASSETS: &str = "batch";
 
     fn gen_proving_task() -> eyre::Result<<Self::Prover as ProverType>::ProvingTask> {
-        #[cfg(feature = "euclidv2")]
-        unreachable!("euclidv2 task is not ready");
 
-        #[cfg(not(feature = "euclidv2"))]
         Ok(read_json_deep(
-            Path::new(PATH_TESTDATA).join("batch-task.json"),
+            Path::new(PATH_TESTDATA).
+            join(phase_base_directory()).
+            join("tasks").
+            join("batch-task.json"),
         )?)
     }
 }
