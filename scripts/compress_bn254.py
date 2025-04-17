3a7db8b0eda78bf5e476fa94e82a2c2e77e9fa32
Zhuo Zhang <mycinbrin@gmail.com>
2025-04-17 13:56:23 +0000

    rewrite build guest

--- scripts/compress_bn254.py
+++ scripts/compress_bn254.py
@@ -1,10 +1,14 @@
 import json 
 import sys
+import re
 
 def compress(arr): 
     return sum(v * pow(2013265921, i) for i, v in enumerate(arr))
 
-arr = json.loads(open(sys.argv[1]).read())
+pattern = r'\[\s*\d+\s*(,\s*\d+\s*)*\]'
+code = open(sys.argv[1]).read()
+matched = re.search(pattern, code).group(0)
+arr = json.loads(matched)
 f = compress(arr)
 h = f.to_bytes(32, 'big').hex()
 print(h)
