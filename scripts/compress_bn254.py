165df451200c0953ad62f62ff7957f2b2eb0319e
Zhang Zhuo <mycinbrin@gmail.com>
2025-05-06 14:44:36 +0800

    clean up scripts

--- scripts/compress_bn254.py
+++ scripts/compress_bn254.py
@@ -1,3 +1,7 @@
+# This script compresses a list of u32 integers into a single hex string (bn254).
+# It reads from generated commitment files, such as crates/circuits/batch-circuit/batch_exe_commit.rs,
+# and outputs the compressed hex string. 
+# It can be used to verify the generated bundle-circuit/digest_{1,2} files.
 import json 
 import sys
 import re
