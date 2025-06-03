00aa6328fddd3abe9cd38ae9fb2d105d6d76a02c
Zhang Zhuo <mycinbrin@gmail.com>
2025-06-03 12:25:35 +0800

    v0.4.2: fix a plonky3 soundness issue (#112)
    * p3 patch
    
    * update scripts
    
    * update openvm and stark-backend
    
    * update Cargo.lock
    
    * simplify scripts/20250508-p3.sh
    
    * feat: make export-onchain-verifier (#114)
    
    * wip
    
    * fixed
    
    ---------
    
    Co-authored-by: Rohit Narurkar <rohit.narurkar@proton.me>

---
+++ scripts/20250508-p3.sh
@@ -0,0 +1,32 @@
+set -ex
+function patch_crates() {
+  sed -i 's#git = "https://github.com/openvm-org/openvm.git", rev = "a0ae88f"#git = "ssh://git@github.com/axiom-crypto/openvm-private.git", branch = "patch-v1.2.0"#' Cargo.toml
+  sed -i 's#git = "https://github.com/Plonky3/Plonky3.git", rev = "1ba4e5c"#git = "ssh://git@github.com/axiom-crypto/plonky3-private.git", rev = "51704e6036fba6edd58022eb0eceb9f44bc941fb"#' Cargo.toml
+  sed -i 's#git = "https://github.com/openvm-org/stark-backend.git", tag = "v1.0.1"#git = "ssh://git@github.com/axiom-crypto/stark-backend-private.git", branch = "patch-v1.1.0"#' Cargo.toml
+}
+
+function stage1_stage2() {
+  #patch_crates
+  echo 'BUILD_STAGES=stage1,stage2' >crates/build-guest/.env
+  bash build-guest-actions-entrypoint.sh
+  #cp Cargo.toml Cargo.toml.gpu
+  #cp Cargo.lock Cargo.lock.gpu
+  #git checkout Cargo.toml Cargo.lock
+}
+
+function stage3() {
+  echo 'BUILD_STAGES=stage3' >crates/build-guest/.env
+  make build-guest
+}
+
+#if ! git diff --quiet || ! git diff --cached --quiet; then
+#  echo "Error: Git has modified tracked files (dirty working tree)." >&2
+#  echo "Either commit, stash, or discard them before proceeding." >&2
+#  exit 1
+#fi
+#if [ "$BUILD_MODE" = "full" ]; then
+stage1_stage2
+#fi
+git checkout origin/master Cargo.toml Cargo.lock
+stage3
+#patch_crates
