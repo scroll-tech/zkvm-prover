0dd7b19590b1b17c5ec5a97fc1b4f638dfd0ce8f
Zhang Zhuo <mycinbrin@gmail.com>
2025-07-03 14:06:23 +0000

    update release.sh

--- release.sh
+++ release.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # release version
+SCROLL_ZKVM_TESTRUN_DIR=`realpath .output/bundle-tests-20250703_1141191`
+SCROLL_ZKVM_VERSION=0.5.0rc0
 if [ -z "${SCROLL_ZKVM_VERSION}" ]; then
   echo "SCROLL_ZKVM_VERSION not set"
   exit 1
@@ -32,18 +34,15 @@ cp ./crates/circuits/batch-circuit/openvm.toml $DIR_OUTPUT/batch/openvm.toml
 
 # copy bundle-program related assets
 cp ./crates/circuits/bundle-circuit/openvm/app.vmexe $DIR_OUTPUT/bundle/app.vmexe
-cp ./crates/circuits/bundle-circuit/openvm/app_euclidv1.vmexe $DIR_OUTPUT/bundle/app_euclidv1.vmexe
 cp ./crates/circuits/bundle-circuit/openvm.toml $DIR_OUTPUT/bundle/openvm.toml
 cp ./crates/circuits/bundle-circuit/openvm/verifier.bin $DIR_OUTPUT/bundle/verifier.bin
 cp ./crates/circuits/bundle-circuit/openvm/verifier.sol $DIR_OUTPUT/bundle/verifier.sol
 xxd -l 32 -p ./crates/circuits/bundle-circuit/digest_1 | tr -d '\n' | awk '{gsub("%", ""); print}' > $DIR_OUTPUT/bundle/digest_1.hex
 xxd -l 32 -p ./crates/circuits/bundle-circuit/digest_2 | tr -d '\n' | awk '{gsub("%", ""); print}' > $DIR_OUTPUT/bundle/digest_2.hex
-xxd -l 32 -p ./crates/circuits/bundle-circuit/digest_1_euclidv1 | tr -d '\n' | awk '{gsub("%", ""); print}' > $DIR_OUTPUT/bundle/digest_1_euclidv1.hex
-xxd -l 32 -p ./crates/circuits/bundle-circuit/digest_2_euclidv1 | tr -d '\n' | awk '{gsub("%", ""); print}' > $DIR_OUTPUT/bundle/digest_2_euclidv1.hex
 
 # copy verifier-only assets
-cp $DIR_INPUT/bundle/root-verifier-vm-config $DIR_OUTPUT/verifier/root-verifier-vm-config
-cp $DIR_INPUT/bundle/root-verifier-committed-exe $DIR_OUTPUT/verifier/root-verifier-committed-exe
+#cp $DIR_INPUT/bundle/root-verifier-vm-config $DIR_OUTPUT/verifier/root-verifier-vm-config
+#cp $DIR_INPUT/bundle/root-verifier-committed-exe $DIR_OUTPUT/verifier/root-verifier-committed-exe
 cp ./crates/circuits/bundle-circuit/openvm/verifier.bin $DIR_OUTPUT/verifier/verifier.bin
 
 # upload to s3
