1aaef1c6c683af42f04588baf34aa8682735b545
Zhang Zhuo <mycinbrin@gmail.com>
2025-07-11 01:48:06 +0000

    upgrade reth, but 2x cycle

--- crates/integration/tests/chunk_circuit.rs
+++ crates/integration/tests/chunk_circuit.rs
@@ -54,7 +54,7 @@ fn test_cycle() -> eyre::Result<()> {
         };
         let (exec_result, gas) = exec_chunk(&task)?;
         let cycle_per_gas = exec_result.total_cycle / gas;
-        assert!(cycle_per_gas < 30);
+        assert!(cycle_per_gas < 100);
         Ok(())
     })?;
     Ok(())
