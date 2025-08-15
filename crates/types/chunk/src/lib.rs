d69306e28616166f95f89930a34dc99d99b5eb53
lightsing <light.tsing@gmail.com>
2025-08-15 08:59:56 +0800

    update based on sbv

--- crates/types/chunk/src/lib.rs
+++ crates/types/chunk/src/lib.rs
@@ -1,10 +1,5 @@
-#![feature(lazy_get)]
-
-mod utils;
-
 mod witness;
 
-pub use utils::{BlockHashProvider, CodeDb, NodesProvider, make_providers};
 pub use witness::{ArchivedChunkWitness, ChunkWitness};
 
 mod execute;
