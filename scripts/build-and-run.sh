0d4c636aa9759f2e2ef22c5a3f841e1d32335d44
Zhang Zhuo <mycinbrin@gmail.com>
2025-08-19 05:48:35 +0000

    sh

--- scripts/build-and-run.sh
+++ scripts/build-and-run.sh
@@ -1,3 +1,3 @@
 set -euxo pipefail
-bash build-guest-actions-entrypoint.sh 2>&1 | tee build.log
+cargo run --release -p scroll-zkvm-build-guest 2>&1 | tee build.log
 make test-e2e-bundle 2>&1 | tee e2e.log
