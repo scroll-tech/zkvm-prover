5824aaf55850c9e9eba7092bc9d3d52acd16071f
Zhang Zhuo <mycinbrin@gmail.com>
2025-09-04 10:25:57 +0800

    move blob_consistency/types.rs

--- crates/types/batch/src/blob_consistency/openvm.rs
+++ crates/types/batch/src/blob_consistency/openvm.rs
@@ -55,43 +55,6 @@ static KZG_G2_SETUP: LazyLock<G2Affine> = LazyLock::new(|| {
 /// The version for KZG as per EIP-4844.
 const VERSIONED_HASH_VERSION_KZG: u8 = 1;
 
-/// Helper trait that provides functionality to convert the given type from native to the desired intrinsic type
-pub trait ToIntrinsic {
-    /// The desired intrinsic type
-    type IntrinsicType;
-
-    /// Convert the given type from native to the desired intrinsic type
-    fn to_intrinsic(&self) -> Self::IntrinsicType;
-}
-
-impl ToIntrinsic for Bls12_381_Fq {
-    type IntrinsicType = Fp;
-
-    fn to_intrinsic(&self) -> Self::IntrinsicType {
-        let bytes = self.to_bytes();
-        Fp::from_le_bytes_unchecked(&bytes)
-    }
-}
-
-impl ToIntrinsic for Bls12_381_G1 {
-    type IntrinsicType = G1Affine;
-
-    fn to_intrinsic(&self) -> Self::IntrinsicType {
-        G1Affine::from_xy_unchecked(self.x.to_intrinsic(), self.y.to_intrinsic())
-    }
-}
-
-impl ToIntrinsic for Bls12_381_G2 {
-    type IntrinsicType = G2Affine;
-
-    fn to_intrinsic(&self) -> Self::IntrinsicType {
-        G2Affine::from_xy_unchecked(
-            Fp2::new(self.x.c0.to_intrinsic(), self.x.c1.to_intrinsic()),
-            Fp2::new(self.y.c0.to_intrinsic(), self.y.c1.to_intrinsic()),
-        )
-    }
-}
-
 /// Verify KZG `proof` that `P(z) == y` where `P` is the EIP-4844 blob polynomial in its evaluation
 /// form, and `commitment` is the KZG commitment to the polynomial `P`.
 ///
