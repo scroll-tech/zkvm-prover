5c361ad2eec9a2f115671d6a4dab1628aae63259
Zhang Zhuo <mycinbrin@gmail.com>
2025-09-16 10:52:45 +0800

    make ValidiumInputs public

--- crates/types/chunk/src/lib.rs
+++ crates/types/chunk/src/lib.rs
@@ -2,7 +2,7 @@ mod types;
 mod witness;
 
 pub use types::validium::SecretKey;
-pub use witness::{ChunkWitness, LegacyChunkWitness};
+pub use witness::{ChunkWitness, LegacyChunkWitness, ValidiumInputs};
 
 mod execute;
 pub use execute::execute;
