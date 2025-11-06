eeb76a9bd44de4589690f7a03f2737f0fff27c0c
Rohit Narurkar <rohit.narurkar@proton.me>
2025-11-06 13:04:40 +0000

    test: update testdata

--- crates/integration/src/testers/chunk.rs
+++ crates/integration/src/testers/chunk.rs
@@ -198,7 +198,7 @@ pub fn preset_chunk() -> ChunkTaskGenerator {
         ForkName::EuclidV1 => (Version::euclid_v1(), 12508460u64..=12508463u64),
         ForkName::EuclidV2 => (Version::euclid_v2(), 1u64..=4u64),
         ForkName::Feynman => (Version::feynman(), 16525000u64..=16525003u64),
-        ForkName::Galileo => (Version::galileo(), 20239157..=20239192),
+        ForkName::Galileo => (Version::galileo(), 20239156..=20239168),
     };
 
     ChunkTaskGenerator {
@@ -262,9 +262,9 @@ pub fn preset_chunk_multiple() -> Vec<ChunkTaskGenerator> {
                 ),
                 ForkName::Galileo => (
                     vec![
-                        20239157..=20239162,
-                        20239163..=20239175,
-                        20239176..=20239192,
+                        20239156..=20239160,
+                        20239161..=20239164,
+                        20239165..=20239168,
                     ],
                     Version::galileo(),
                 ),
