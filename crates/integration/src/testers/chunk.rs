fb8ad5b1f51c7e31094b0466a84c09ae4e8e770f
Rohit Narurkar <rohit.narurkar@proton.me>
2025-11-06 10:23:32 +0000

    test: remove witness for block xxx156

--- crates/integration/src/testers/chunk.rs
+++ crates/integration/src/testers/chunk.rs
@@ -198,7 +198,7 @@ pub fn preset_chunk() -> ChunkTaskGenerator {
         ForkName::EuclidV1 => (Version::euclid_v1(), 12508460u64..=12508463u64),
         ForkName::EuclidV2 => (Version::euclid_v2(), 1u64..=4u64),
         ForkName::Feynman => (Version::feynman(), 16525000u64..=16525003u64),
-        ForkName::Galileo => (Version::galileo(), 20239156..=20239192),
+        ForkName::Galileo => (Version::galileo(), 20239157..=20239192),
     };
 
     ChunkTaskGenerator {
@@ -262,7 +262,7 @@ pub fn preset_chunk_multiple() -> Vec<ChunkTaskGenerator> {
                 ),
                 ForkName::Galileo => (
                     vec![
-                        20239156..=20239162,
+                        20239157..=20239162,
                         20239163..=20239175,
                         20239176..=20239192,
                     ],
