3cbff7466faad7ba2ac495adfdea6c257ff81f76
Ho <noelwei@gmail.com>
2025-11-17 16:58:31 +0900

    [Feat] Support axiom cli sdk (#206)
    * support axiom sdk
    
    * wip
    
    * update lock
    
    * upgrade axiom sdk to 1.0.6
    
    * WIP
    
    * fix building
    
    * revert to 1.4.0
    
    * save work
    
    * ok
    
    * rollback to v1.4.1
    
    * use toml download from api
    
    * use axiom openvm toml
    
    * add config id to types
    
    * use config id from scroll_zkvm_types
    
    * refactor
    
    * refactor
    
    * 1.4 verifier
    
    * add batch e2e
    
    * update ref
    
    * add e2e
    
    * revert to 1.4.1
    
    * fmt & minor changes
    
    * re-export
    
    * update lock
    
    * fix rebase master
    
    * revert openvm.toml since actually not changed
    
    * remove unneeded
    
    * optional upload
    
    * clippy
    
    * fix
    
    * update commitment
    
    * fix
    
    * fix
    
    * apply review
    
    * remove upload from build-guest
    
    * seperate upload program
    
    * clippy
    
    * apply review & lints
    
    * update commitment
    
    ---------
    
    Co-authored-by: lightsing <light.tsing@gmail.com>
    Co-authored-by: Zhang Zhuo <mycinbrin@gmail.com>

---
+++ crates/types/src/openvm.rs
@@ -0,0 +1,48 @@
+use openvm_native_recursion::hints::Hintable;
+use openvm_stark_sdk::openvm_stark_backend::p3_field::PrimeField32;
+use serde::{Deserialize, Serialize};
+
+/// Input structure for OpenVM input json
+///
+/// ```json
+/// {
+///   "input": [ "0x...", "0x...", ... ]
+/// }
+/// ```
+///
+/// Reference: https://github.com/openvm-org/openvm/blob/7e9488992a74d49fa697359681cd2a7e768b90ef/crates/cli/src/input.rs#L82-L115
+#[derive(Clone, Default, Serialize, Deserialize)]
+pub struct OpenVMInput {
+    input: Vec<String>,
+}
+
+impl super::ProvingTask {
+    pub fn build_openvm_input(&self) -> OpenVMInput {
+        let mut input = Vec::new();
+
+        // Encode witness entries (0x01 | bytes)
+        for w in self.serialized_witness.iter() {
+            let mut buf = Vec::with_capacity(1 + w.len());
+            buf.push(0x01);
+            buf.extend_from_slice(w);
+            input.push(format!("0x{}", hex::encode(&buf)));
+        }
+
+        // Encode proof fields (0x02 | u32_le_bytes...)
+        for field in self
+            .aggregated_proofs
+            .iter()
+            .flat_map(|proof| proof.proofs[0].write())
+        {
+            let mut buf = Vec::with_capacity(1 + 4 * field.len());
+            buf.push(0x02);
+            for f in field {
+                let v: u32 = f.as_canonical_u32();
+                buf.extend_from_slice(&v.to_le_bytes());
+            }
+            input.push(format!("0x{}", hex::encode(&buf)));
+        }
+
+        OpenVMInput { input }
+    }
+}
