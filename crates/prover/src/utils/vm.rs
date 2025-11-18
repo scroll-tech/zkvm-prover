5f0509c4a5c603aa0e06f6364605b1dad3a98a05
Zhang Zhuo <mycinbrin@gmail.com>
2025-11-18 10:24:47 +0800

    refactor sdk cycle counting (#222)

--- crates/prover/src/utils/vm.rs
+++ crates/prover/src/utils/vm.rs
@@ -1,12 +1,4 @@
-use std::sync::Arc;
-
-use openvm_circuit::{
-    arch::{VmExecutor, instructions::exe::VmExe},
-    system::{
-        memory::merkle::public_values::extract_public_values, program::trace::VmCommittedExe,
-    },
-};
-use openvm_sdk::{F, SC, StdIn, config::SdkVmConfig};
+use openvm_sdk::{Sdk, StdIn, types::ExecutableFormat};
 
 use crate::Error;
 
@@ -16,28 +8,15 @@ pub struct ExecutionResult {
     pub public_values: Vec<u8>,
 }
 
-#[derive(Default)]
-pub struct DebugInput {
-    pub mock_prove: bool,
-    pub commited_exe: Option<Arc<VmCommittedExe<SC>>>,
-}
-
 pub fn execute_guest(
-    vm_config: SdkVmConfig,
-    exe: &VmExe<F>,
+    sdk: &Sdk,
+    exe: impl Into<ExecutableFormat>,
     stdin: &StdIn,
 ) -> Result<ExecutionResult, Error> {
-    let executor = VmExecutor::new(vm_config.clone()).expect("invalid config");
-    let instance = executor.instance(exe).expect("invalid exe");
-
-    let state = instance
-        .execute(stdin.clone(), None)
+    let (public_values, (_cost, total_cycle)) = sdk
+        .execute_metered_cost(exe, stdin.clone())
         .map_err(|e| Error::GenProof(e.to_string()))?;
-    let total_cycle = state.instret();
-    let final_memory = state.memory;
 
-    let public_values: Vec<u8> =
-        extract_public_values(vm_config.as_ref().num_public_values, &final_memory.memory);
     tracing::debug!(name: "public_values after guest execution", ?public_values);
     if public_values.iter().all(|x| *x == 0) {
         return Err(Error::GenProof("public_values are all 0s".to_string()));
