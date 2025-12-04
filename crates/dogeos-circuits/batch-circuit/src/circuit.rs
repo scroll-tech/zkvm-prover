a4430ffb47e36074f129dd34eee349906a8b38b3
lightsing <light.tsing@gmail.com>
2025-12-04 14:18:01 +0800

    copy circuit

---
+++ crates/dogeos-circuits/batch-circuit/src/circuit.rs
@@ -0,0 +1,98 @@
+use alloy_primitives::B256;
+use scroll_zkvm_types_batch::BatchWitness;
+use scroll_zkvm_types_circuit::{
+    AggCircuit, AggregationInput, Circuit, ProgramCommitment,
+    io::read_witnesses,
+    public_inputs::{
+        Version,
+        scroll::{
+            batch::{BatchInfo, VersionedBatchInfo},
+            chunk::VersionedChunkInfo,
+        },
+    },
+};
+
+use crate::child_commitments;
+
+#[allow(unused_imports, clippy::single_component_path_imports)]
+use {
+    openvm_algebra_guest::{IntMod, field::FieldExtension},
+    openvm_ecc_guest::AffinePoint,
+    openvm_keccak256_guest, // trigger extern native-keccak256
+    openvm_pairing::bls12_381::{Bls12_381, Bls12_381G1Affine, Fp, Fp2},
+    openvm_sha256_guest,
+};
+
+openvm::init!();
+
+pub struct BatchCircuit;
+
+impl Circuit for BatchCircuit {
+    type Witness = BatchWitness;
+
+    type PublicInputs = VersionedBatchInfo;
+
+    fn read_witness_bytes() -> Vec<u8> {
+        read_witnesses()
+    }
+
+    fn deserialize_witness(witness_bytes: &[u8]) -> Self::Witness {
+        let config = bincode::config::standard();
+        let (witness, _): (Self::Witness, _) =
+            bincode::serde::decode_from_slice(witness_bytes, config)
+                .expect("BatchCircuit: deserialisation of witness bytes failed");
+        witness
+    }
+
+    fn validate(witness: Self::Witness) -> Self::PublicInputs {
+        let version = Version::from(witness.version);
+        assert_eq!(version.fork, witness.fork_name);
+
+        (BatchInfo::from(&witness), version)
+    }
+}
+
+impl AggCircuit for BatchCircuit {
+    type AggregatedPublicInputs = VersionedChunkInfo;
+
+    fn verify_commitments(commitment: &ProgramCommitment) {
+        assert_eq!(
+            commitment.vm,
+            child_commitments::VM_COMMIT,
+            "mismatch chunk-proof leaf commitment: expected={:?}, got={:?}",
+            child_commitments::VM_COMMIT,
+            commitment.vm,
+        );
+        assert_eq!(
+            commitment.exe,
+            child_commitments::EXE_COMMIT,
+            "mismatch chunk-proof exe commitment: expected={:?}, got={:?}",
+            child_commitments::EXE_COMMIT,
+            commitment.exe,
+        );
+    }
+
+    fn aggregated_public_inputs(witness: &Self::Witness) -> Vec<Self::AggregatedPublicInputs> {
+        let version = Version::from(witness.version);
+        witness
+            .chunk_infos
+            .iter()
+            .cloned()
+            .map(|chunk_info| (chunk_info, version))
+            .collect()
+    }
+
+    fn aggregated_pi_hashes(proofs: &[AggregationInput]) -> Vec<alloy_primitives::B256> {
+        proofs
+            .iter()
+            .map(|proof| {
+                let transformed = proof
+                    .public_values
+                    .iter()
+                    .map(|&val| u8::try_from(val).expect("0 < public value < 256"))
+                    .collect::<Vec<u8>>();
+                B256::from_slice(transformed.as_slice())
+            })
+            .collect()
+    }
+}
---
+++ crates/dogeos-circuits/bundle-circuit/src/circuit.rs
@@ -0,0 +1,91 @@
+use alloy_primitives::B256;
+use scroll_zkvm_types_bundle::BundleWitness;
+use scroll_zkvm_types_circuit::{
+    AggCircuit, AggregationInput, Circuit, ProgramCommitment,
+    io::read_witnesses,
+    public_inputs::{
+        Version,
+        scroll::{
+            batch::VersionedBatchInfo,
+            bundle::{BundleInfo, VersionedBundleInfo},
+        },
+    },
+};
+
+use crate::child_commitments;
+
+#[allow(unused_imports, clippy::single_component_path_imports)]
+use openvm_keccak256_guest;
+
+#[derive(Default)]
+pub struct BundleCircuit;
+
+impl Circuit for BundleCircuit {
+    type Witness = BundleWitness;
+
+    type PublicInputs = VersionedBundleInfo;
+
+    fn read_witness_bytes() -> Vec<u8> {
+        read_witnesses()
+    }
+
+    fn deserialize_witness(witness_bytes: &[u8]) -> Self::Witness {
+        let config = bincode::config::standard();
+        let (witness, _): (Self::Witness, _) =
+            bincode::serde::decode_from_slice(witness_bytes, config)
+                .expect("BundleCircuit: deserialization of witness bytes failed");
+        witness
+    }
+
+    fn validate(witness: Self::Witness) -> Self::PublicInputs {
+        let version = Version::from(witness.version);
+        assert_eq!(version.fork, witness.fork_name);
+
+        (BundleInfo::from(&witness), version)
+    }
+}
+
+impl AggCircuit for BundleCircuit {
+    type AggregatedPublicInputs = VersionedBatchInfo;
+
+    fn verify_commitments(commitment: &ProgramCommitment) {
+        assert_eq!(
+            commitment.vm,
+            child_commitments::VM_COMMIT,
+            "mismatch batch-proof leaf commitment: expected={:?}, got={:?}",
+            child_commitments::VM_COMMIT,
+            commitment.vm,
+        );
+        assert_eq!(
+            commitment.exe,
+            child_commitments::EXE_COMMIT,
+            "mismatch batch-proof exe commitment: expected={:?}, got={:?}",
+            child_commitments::EXE_COMMIT,
+            commitment.exe,
+        );
+    }
+
+    fn aggregated_public_inputs(witness: &Self::Witness) -> Vec<Self::AggregatedPublicInputs> {
+        let version = Version::from(witness.version);
+        witness
+            .batch_infos
+            .iter()
+            .cloned()
+            .map(|batch_info| (batch_info, version))
+            .collect()
+    }
+
+    fn aggregated_pi_hashes(proofs: &[AggregationInput]) -> Vec<B256> {
+        proofs
+            .iter()
+            .map(|proof| {
+                let transformed = proof
+                    .public_values
+                    .iter()
+                    .map(|&val| u8::try_from(val).expect("0 < public value < 256"))
+                    .collect::<Vec<u8>>();
+                B256::from_slice(transformed.as_slice())
+            })
+            .collect()
+    }
+}
