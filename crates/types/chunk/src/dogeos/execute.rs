07d9db16708a2be79c17d7c1dc78518b0bf2b8b2
lightsing <light.tsing@gmail.com>
2025-12-05 10:08:18 +0800

    add ref

--- crates/types/chunk/src/dogeos/execute.rs
+++ crates/types/chunk/src/dogeos/execute.rs
@@ -64,17 +64,49 @@ fn verify_deposits(
         .zip_eq(l1_messages) {
         let relay_call: relayMessageCall = relayMessageCall::abi_decode(l1_message.input.as_ref())
             .map_err(|e| format!("dogeos relay call decode failed: {e}"))?;
-        // relay call checks
+        // -- l1 message checks --
+        // possibly redundant checks, kept for clarity
+
+        // ref: https://github.com/DogeOS69/dogeos-core/blob/73ff17223d3bdb473b164bd87798207b5df7275e/crates/l1_interface/src/state/log_generator.rs#L531-L552
+        // assert_eq!(l1_message.sender, aliased_l1_messenger);
+
+        // ref:
+        // - https://github.com/DogeOS69/dogeos-core/blob/73ff17223d3bdb473b164bd87798207b5df7275e/crates/l1_interface/src/state/log_generator.rs#L507-L512
+        // - https://github.com/DogeOS69/dogeos-core/blob/73ff17223d3bdb473b164bd87798207b5df7275e/crates/l1_interface/src/state/log_generator.rs#L556
+        // assert_eq!(l1_message.to, messenger_target);
+
+        // ref: https://github.com/DogeOS69/dogeos-core/blob/73ff17223d3bdb473b164bd87798207b5df7275e/crates/l1_interface/src/state/log_generator.rs#L557
+        // assert_eq!(l1_message.value, U256::ZERO);
+
+        // -- relay call checks --
+
+        // ref:
+        // - https://github.com/DogeOS69/dogeos-core/blob/73ff17223d3bdb473b164bd87798207b5df7275e/crates/l1_interface/src/state/log_generator.rs#L490
+        // - https://github.com/DogeOS69/dogeos-core/blob/d0f71b8596f116d7fef5859c5a44eb385bf55499/crates/l1_interface/src/deposit/calldata_builder.rs#L102
         assert_eq!(relay_call.sender, deposit.txid[..20], "dogeos synthetic relay call sender mismatch");
+
+        // ref: https://github.com/DogeOS69/dogeos-core/blob/d0f71b8596f116d7fef5859c5a44eb385bf55499/crates/l1_interface/src/deposit/calldata_builder.rs#L103
         assert_eq!(relay_call.target, MOAT_CONTRACT_ADDRESS, "dogeos relay call target mismatch");
+
+        // ref: https://github.com/DogeOS69/dogeos-core/blob/73ff17223d3bdb473b164bd87798207b5df7275e/crates/l1_interface/src/state/log_generator.rs#L503
         let amount = U256::from(bridge_transforms::convert_doge_to_eth_units(deposit.amount_sats));
+        // ref: https://github.com/DogeOS69/dogeos-core/blob/d0f71b8596f116d7fef5859c5a44eb385bf55499/crates/l1_interface/src/deposit/calldata_builder.rs#L104
         assert_eq!(relay_call.value, amount, "dogeos relay call amount mismatch");
         // nonce/queueIndex check is skipped, as it's guaranteed by rolling hash
 
         let moat_call: handleL1MessageCall = handleL1MessageCall::abi_decode(relay_call.message.as_ref())
             .map_err(|e| format!("dogeos moat call decode failed: {e}"))?;
-        // moat call checks
+
+        // -- moat call checks --
+
+        // ref:
+        // - https://github.com/DogeOS69/dogeos-core/blob/73ff17223d3bdb473b164bd87798207b5df7275e/crates/l1_interface/src/state/log_generator.rs#L500
+        // - https://github.com/DogeOS69/dogeos-core/blob/d0f71b8596f116d7fef5859c5a44eb385bf55499/crates/l1_interface/src/deposit/calldata_builder.rs#L96
         assert_eq!(moat_call.target, deposit.evm_recipient, "dogeos deposit recipient mismatch");
+
+        // ref:
+        // - https://github.com/DogeOS69/dogeos-core/blob/73ff17223d3bdb473b164bd87798207b5df7275e/crates/l1_interface/src/state/log_generator.rs#L514-L518
+        // - https://github.com/DogeOS69/dogeos-core/blob/d0f71b8596f116d7fef5859c5a44eb385bf55499/crates/l1_interface/src/deposit/calldata_builder.rs#L96
         assert_eq!(moat_call.depositID, deposit.txid);
     }
 
