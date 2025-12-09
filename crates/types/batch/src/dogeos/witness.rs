2923257ebaf73183c90bcd20b2656cb55f675a5b
lightsing <light.tsing@gmail.com>
2025-12-09 09:36:00 +0800

    add DaInclusionVerifier

--- crates/types/batch/src/dogeos/witness.rs
+++ crates/types/batch/src/dogeos/witness.rs
@@ -1,7 +1,11 @@
+use bridge_adapters_zk::serde::SerdeWrapper;
+use bridge_adapters_zk::{StepInputEnvelope, ZkVerifierExt};
+use bridge_core::VerifierContext;
 use types_base::aggregation::{AggregationInput, ProofCarryingWitness};
 use types_base::public_inputs::dogeos::batch::DogeOsBatchInfo;
 use types_base::public_inputs::dogeos::chunk::DogeOsChunkInfoExtras;
 use types_base::public_inputs::scroll;
+use bridge_steps_da::DaInclusionVerifier;
 
 /// Witness to the batch circuit.
 #[derive(Clone, Debug, serde::Deserialize, serde::Serialize)]
@@ -15,7 +19,10 @@ pub struct DogeOsBatchWitness {
 #[derive(Clone, Debug, serde::Deserialize, serde::Serialize)]
 pub struct DogeOsBatchWitnessExtras {
     pub chunk_info_extras: Vec<DogeOsChunkInfoExtras>,
-    pub blobstream: openvm_blobstream::GuestInput,
+    pub verifier_context: SerdeWrapper<VerifierContext>,
+    pub inclusion: SerdeWrapper<StepInputEnvelope<DaInclusionVerifier>>,
+    // TODO: to be decided later how to handle celestia consensus
+    // pub blobstream: openvm_blobstream::GuestInput,
 }
 
 impl ProofCarryingWitness for DogeOsBatchWitness {
@@ -26,9 +33,14 @@ impl ProofCarryingWitness for DogeOsBatchWitness {
 
 impl From<&DogeOsBatchWitness> for DogeOsBatchInfo {
     fn from(witness: &DogeOsBatchWitness) -> Self {
-        let scroll_batch_info = scroll::batch::BatchInfo::from(&witness.inner);
+        DaInclusionVerifier.verify_envelope(
+            &witness.extras.inclusion,
+            &witness.extras.verifier_context,
+        ).expect("failed to verify inclusion proof");
 
-        // TODO: verify blobstream DA proofs
+        // TODO: verifying mapping between extras.chunk_info_extras and inner.chunks
+
+        let scroll_batch_info = scroll::batch::BatchInfo::from(&witness.inner);
 
         DogeOsBatchInfo {
             inner: scroll_batch_info,
