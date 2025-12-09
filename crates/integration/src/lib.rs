1c8167e2931fa2719ec4f58c9e4a2951ed607ceb
lightsing <light.tsing@gmail.com>
2025-12-09 16:07:32 +0800

    override

--- crates/integration/src/lib.rs
+++ crates/integration/src/lib.rs
@@ -50,7 +50,7 @@ pub fn testing_hardfork() -> ForkName {
 
 /// Test settings (version).
 pub fn testing_version() -> Version {
-    Version::galileo_v2()
+    Version::feynman()
 }
 
 pub fn testing_version_validium() -> Version {
