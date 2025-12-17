545ac8f7eb105f8ae8caffe79b6f0418e990d37d
Zhang Zhuo <mycinbrin@gmail.com>
2025-12-17 16:19:02 +0800

    clippy

--- crates/types/src/zkvm.rs
+++ crates/types/src/zkvm.rs
@@ -4,7 +4,7 @@ use openvm_sdk::keygen::AggProvingKey;
 
 /// Proving key for STARK aggregation. Primarily used to aggregate
 /// [continuation proofs][openvm_sdk::prover::vm::ContinuationVmProof].
-pub static AGG_STARK_PROVING_KEY: Lazy<AggProvingKey> = Lazy::new(|| build_agg_pk());
+pub static AGG_STARK_PROVING_KEY: Lazy<AggProvingKey> = Lazy::new(build_agg_pk);
 
 fn build_agg_pk() -> AggProvingKey {
     Sdk::riscv32().agg_pk().clone()
