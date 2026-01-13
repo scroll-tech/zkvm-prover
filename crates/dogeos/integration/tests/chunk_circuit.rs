412e9e7a46f9a7ef4ffa239a43d9bdf6a61add27
lightsing <light.tsing@gmail.com>
2026-01-13 17:10:02 +0800

    add test-single-chunk

--- crates/dogeos/integration/tests/chunk_circuit.rs
+++ crates/dogeos/integration/tests/chunk_circuit.rs
@@ -1,5 +1,5 @@
 use dogeos_zkvm_integration::testers::chunk::{ChunkProverTester, exec_chunk, mock_chunk_witness};
-use scroll_zkvm_integration::ProverTester;
+use scroll_zkvm_integration::{prove_verify, ProverTester};
 
 #[test]
 fn test_execute() -> eyre::Result<()> {
@@ -7,6 +7,17 @@ fn test_execute() -> eyre::Result<()> {
 
     let wit = mock_chunk_witness()?;
     exec_chunk(&wit)?;
+
+    Ok(())
+}
+
+#[test]
+fn setup_prove_verify_single() -> eyre::Result<()> {
+    ChunkProverTester::setup(true)?;
+    let mut prover = ChunkProverTester::load_prover(false)?;
+
+    let wit = mock_chunk_witness()?;
+    let _ = prove_verify::<ChunkProverTester>(&mut prover, &wit, &[])?;
 
     Ok(())
 }
