bb1a23d50c3729f70b534327b37fb6f5ce7cf20b
Ho <noelwei@gmail.com>
2026-03-02 20:21:25 +0900

    [Fix] metered execution suspend before guest program completed (#246)
    * better guest exec
    
    * lint
    
    * update according to reviews

--- crates/integration/src/lib.rs
+++ crates/integration/src/lib.rs
@@ -357,8 +357,14 @@ pub fn tester_execute<T: ProverTester>(
             .map(|p| p.as_stark_proof().expect("must be stark proof")),
     )?;
 
+    let app_vm_config = app_config.app_vm_config.clone();
     let sdk = Sdk::new(app_config)?;
-    let ret = scroll_zkvm_prover::utils::vm::execute_guest(&sdk, app_exe, &stdin)?;
+    let ret = scroll_zkvm_prover::utils::vm::execute_guest(
+        &sdk,
+        app_vm_config.as_ref(),
+        app_exe,
+        &stdin,
+    )?;
     Ok(ret)
 }
 
--- crates/prover/src/utils/vm.rs
+++ crates/prover/src/utils/vm.rs
@@ -1,6 +1,8 @@
-use openvm_sdk::{Sdk, StdIn, types::ExecutableFormat};
-
-use crate::Error;
+use openvm_circuit::{
+    arch::{SystemConfig, VirtualMachineError},
+    system::memory::merkle::public_values::extract_public_values,
+};
+use openvm_sdk::{Sdk, SdkError, StdIn, types::ExecutableFormat};
 
 pub struct ExecutionResult {
     pub total_cycle: u64,
@@ -8,20 +10,69 @@ pub struct ExecutionResult {
     pub public_values: Vec<u8>,
 }
 
+// the 100* current cost / ratio for chunk circuit, use to estimated the max cycles
+// which an metered execution can handle
+const COST_CYCLE_RATIO: u64 = 87u64;
+
+// Execute the guest program using the metered executor first to measure actual cycles.
+// If the execution exceeds the maximum cost allowed by the metered executor,
+// we re-execute the program using the normal executor (execute_e1), which has no limitations
+// on the size of the execution process.
 pub fn execute_guest(
     sdk: &Sdk,
+    sys_config: &SystemConfig,
     exe: impl Into<ExecutableFormat>,
-    stdin: &StdIn,
-) -> Result<ExecutionResult, Error> {
-    let (public_values, (_cost, total_cycle)) = sdk
-        .execute_metered_cost(exe, stdin.clone())
-        .map_err(|e| Error::GenProof(e.to_string()))?;
+    inputs: &StdIn,
+) -> Result<ExecutionResult, SdkError> {
+    let app_prover = sdk.app_prover(exe)?;
 
-    tracing::debug!(name: "public_values after guest execution", ?public_values);
+    let vm = app_prover.vm();
+    let exe = app_prover.exe();
+
+    let ctx = vm.build_metered_cost_ctx();
+    let preset_max_cost = ctx.max_execution_cost * 2;
+    let estimated_max_cycles = preset_max_cost / COST_CYCLE_RATIO;
+    tracing::info!("Double preset max cost to ({preset_max_cost}) for metering execution");
+    let ctx = ctx.with_max_execution_cost(preset_max_cost);
+    let interpreter = vm
+        .metered_cost_interpreter(&exe)
+        .map_err(VirtualMachineError::from)?;
+
+    let (ctx, final_state) = interpreter
+        .execute_metered_cost(inputs.clone(), ctx)
+        .map_err(VirtualMachineError::from)?;
+    let mut total_cycle = ctx.instret;
+
+    let mut public_values =
+        extract_public_values(sys_config.num_public_values, &final_state.memory.memory);
+
     if public_values.iter().all(|x| *x == 0) {
-        return Err(Error::GenProof("public_values are all 0s".to_string()));
+        if ctx.cost < ctx.max_execution_cost {
+            return Err(SdkError::Other(eyre::eyre!(
+                "public_values are all 0s for unexpected reason"
+            )));
+        }
+
+        tracing::warn!(
+            "Large execution exceed limit of metered execution, cycle is expected to >{estimated_max_cycles}"
+        );
+        let exe = sdk.convert_to_exe(exe)?;
+        let instance = vm.interpreter(&exe).map_err(VirtualMachineError::from)?;
+        let final_memory = instance
+            .execute(inputs.clone(), None)
+            .map_err(VirtualMachineError::from)?
+            .memory;
+        public_values = extract_public_values(sys_config.num_public_values, &final_memory.memory);
+        total_cycle = estimated_max_cycles;
+
+        if public_values.iter().all(|x| *x == 0) {
+            return Err(SdkError::Other(eyre::eyre!(
+                "public_values are all 0s upon execute_e1"
+            )));
+        }
     }
 
+    tracing::debug!(name: "public_values after guest execution: {:?}", ?public_values);
     Ok(ExecutionResult {
         total_cycle,
         public_values,
